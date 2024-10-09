6c257d8f2267648ec8bc915f9e3b1a2f303a8980
johncorner <virus1993@icloud.com>
2024-10-09 11:29:07 -0400

    remove AsyncExtensions

--- Dependencies/Sources/DownloadManager/DownloadManager.swift
+++ Dependencies/Sources/DownloadManager/DownloadManager.swift
@@ -6,7 +6,7 @@
 //
 
 import Foundation
-import AsyncExtensions
+//import AsyncExtensions
 
 @available(macOS 11.0, iOS 14.0, tvOS 14.0, watchOS 7.0, *)
 public final class DownloadManager: Sendable {
